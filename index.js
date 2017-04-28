19fb278dcdeea2b27af322c58609a658357980d7
Joseph Karp <joe12345hd@yahoo.com>
2017-04-27 22:23:50 -0400

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by joe12 on 4/27/2017.
+ */
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const path = require('path');
+const io = require('socket.io')(server);
+
+io.on('connection',function(socket)
+{
+    console.log(socket.id);
+});0
+
+
+//sets up view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/views'));
+app.use('/',express.static(path.join(__dirname, 'public')));
+
+app.get('/',function(req,res,next)
+{
+    res.render('index',{serverIP:'http://127.0.0.1:3001'})
+});
+
+
+server.listen(3001);
---
+++ public/javascripts/client.js
@@ -0,0 +1,5 @@
+/**
+ * Created by joe12 on 4/27/2017.
+ */
+alert('hello');
+let socket = io(ip);
