2c347e3e4b950f69d658a9b7a861d0a3339313d8
Joseph Karp <joe12345hd@yahoo.com>
2017-05-15 21:19:01 -0400

    added new tank

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -8,19 +8,36 @@ socket.on('update',function(raw)
     console.log(raw);
 });
 
-// let renderer = PIXI.autoDetectRenderer(256, 256);
-//
-// document.body.appendChild(renderer.view);
-//
-// let stage = new PIXI.Container();
-//
-// PIXI.loader.add("mainTank","images/tank.png")
-//     .load(function()
-//     {
-//         let tank = new PIXI.Sprite(PIXI.loader.resources["images/tank.png"].texture);
-//         stage.addChild(tank);
-//         renderer.render(stage);
-//     });
+const app = new PIXI.Application();
+
+document.body.appendChild(app.view);
+app.stop();
+
+PIXI.loader.add("mainTank","images/tank.png")
+    .load(function()
+    {
+        let baseTexture = PIXI.loader.resources["mainTank"].texture.baseTexture;
+        console.log(baseTexture);
+        let textures = [];
+        for(let i = 0; i < 4;i++)
+        {
+            for(let j = 0; j < 4; j++)
+            {
+                textures.push(new PIXI.Texture(baseTexture,new PIXI.Rectangle((j*64),(i*64),64,64)));
+            }
+        }
+        let tank = new PIXI.extras.AnimatedSprite(textures.slice(0,4));
+
+        tank.gotoAndPlay(0);
+        tank.animationSpeed = .4;
+        app.stage.addChild(tank);
+        console.log("here");
+        app.ticker.add(function()
+        {
+           tank.animationSpeed -= .001;
+        });
+        app.start();
+    });
 
 
 
